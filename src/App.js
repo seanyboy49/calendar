68e3538be8eb8a9c6639c53b6c4f18a4ea33acb3
Sean Lee <seanboramlee@gmail.com>
2017-10-10 17:20:25 -0700

    modify getMonth method but doesn't yet work fully when moving to next year

--- src/App.js
+++ src/App.js
@@ -17,8 +17,7 @@ class App extends Component {
 
     this.getMonth=this.getMonth.bind(this);
     this.clickRight=this.clickRight.bind(this);
-
-
+    this.clickLeft = this.clickLeft.bind(this);
   }
 
   componentWillMount() {
@@ -26,28 +25,38 @@ class App extends Component {
   }
 
   clickRight() {
-    const monthArray = this.props.monthArray;
-    const nextMonth = this.state.currentMonth + 1
-    const nextMonthName = monthArray[nextMonth];
-    this.setState({ currentMonth: nextMonth})
-    this.setState({ currentMonthName: nextMonthName})
-
-    this.getDays(nextMonth, 2017)
-    const date = new Date();
-
-    const firstDay = new Date(date.getFullYear(), nextMonth, 1).getDay();
-    console.log("click Right")
-    console.log({firstDay})
-
-    this.setState( { firstDay: firstDay })
-    console.log(this.state.days);
+    this.getMonth(1);
   }
 
-  getMonth() {
+  clickLeft() {
+    this.getMonth(-1);
+  }
+
+  getMonth(opt) {
+    const yearNav = opt || 0;
+    console.log(yearNav)
     const date = new Date();
     const monthArray = this.props.monthArray;
-    const month = date.getMonth();
-    const year = date.getFullYear();
+    let year = date.getFullYear();
+
+    let month;
+      if (this.state.currentMonth !== null) {
+        if (this.state.currentMonth < 11) {
+          console.log("month < 12")
+         month = this.state.currentMonth + yearNav;
+        } else {
+          console.log("month = 0")
+          month = 0;
+          year +=1;
+        }
+      } else {
+        console.log("first time getting month")
+        month = date.getMonth();
+      }
+
+      console.log({month})
+    
+
     const monthName = monthArray[month];
     this.setState({ currentMonth: month})
     this.setState({ currentMonthName: monthName})
@@ -70,7 +79,7 @@ class App extends Component {
         <div className="app">
           <Month month={this.state.currentMonth} monthName={this.state.currentMonthName}/>
           <div className="date-picker">
-            <div className="left-arrow"></div>
+          <div onClick={this.clickLeft}className="left-arrow"></div>
             <div className="current-month"></div>
             <div onClick={this.clickRight} className="right-arrow"></div>
           </div>
